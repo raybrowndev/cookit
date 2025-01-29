fe128fccf63007f1edabf00b7af059025ed80f5b
Ray Brown <raybrown.dev@gmail.com>
2025-01-29 01:14:09 +0000

    spacing change to landing

--- app/page.tsx
+++ app/page.tsx
@@ -14,16 +14,17 @@ export default function Home() {
     
     <div className="grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
     {/* Main Container */}
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <main className="items-center sm:items-start text-center">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row px-12">
-          <Heading>This is a heading</Heading>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, accusamus nemo. Praesentium repellat aliquam nostrum nihil, a molestiae, dolores quo labore tenetur, magni eaque commodi voluptatibus perspiciatis quod. Dignissimos, quisquam.</p>
-          <Button>Click Here</Button>
-        </div>
+      <div className="px-12 py-6 space-y-8"> 
+        <Heading>This is a heading</Heading>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, accusamus nemo. Praesentium repellat aliquam nostrum nihil, a molestiae, dolores quo labore tenetur, magni eaque commodi voluptatibus perspiciatis quod. Dignissimos, quisquam.</p>
+        <Button>Click Here</Button>
+      </div>
 
-         
-      </main>
+    </main>
+
+
       
     </div>
     <Footer />
